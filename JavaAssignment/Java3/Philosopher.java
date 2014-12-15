c79d343d10e89ad7761c3dce2a516d00d43eb335
naveen-webonise <naveen.patil@weboniselab.com>
2014-12-15 09:59:57 +0530

    used wait and notify

--- JavaAssignment/Java3/Philosopher.java
+++ JavaAssignment/Java3/Philosopher.java
@@ -1,73 +1,10 @@
-class ThreadImplementerInterface extends Thread{
-	private Fork left,right;
 
-	
-	public ThreadImplementerInterface(Fork left, Fork right){
-		this.left = left;
-		this.right= right;
-
-	}
-	@Override
-	public void run(){
-		
-
-		
-			
-				
-			while(true){
-				if(left.forkUse==0 && right.forkUse==0){
-					this.eat(left,right);
-				}
-				else
-					this.think();
-			}
-			
-	}
-	public void eat(Fork left,Fork right){
-		synchronized(left){	
-			synchronized(right){
-			try {
-				
-				System.out.println("Philosopher "+Thread.currentThread().getName()+"is eating");
-				left.forkUse=1;
-				right.forkUse=1;
-				Thread.sleep(50);
-				
-				
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			}
-		
-		}
-	}
-	public void think(){
-		try {
-			
-			
-			Thread.currentThread().sleep(50);
-			left.forkUse=0;
-			right.forkUse=0;
-			
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	}
-	
-class Fork{
-	int forkUse=0;
-	
-}
 public class Philosopher {
-	
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
 		Fork[] fork = new Fork[5];
         for (int i = 0; i <5; i++) {
@@ -85,7 +22,7 @@ public class Philosopher {
         thread2.setName("two");
         thread3.setName("three");
         thread4.setName("four");
-        thread5.setName("Five");
+        thread5.setName("five");
 	        	
 	    		thread1.start();
 	    		thread2.start();
@@ -95,8 +32,81 @@ public class Philosopher {
 	    		thread5.start();
 	    }
 		
+
+}
+class ThreadImplementerInterface extends Thread{
+	private Fork left,right;
+
+	
+	public ThreadImplementerInterface(Fork left, Fork right){
+		this.left = left;
+		this.right= right;
+		
+
+	}
+	@Override
+	public void run(){
+		
+
+		
+			
+				
+			while(true){
+				
+					eat(left,right);
+					try{
+						sleep(50);
+					}
+					catch(InterruptedException e){
+						
+					}
+					think();
+					try{
+						sleep(50);
+					}
+					catch(InterruptedException e){
+						
+					}
+			
+			}
+	}
+	public synchronized void eat(Fork left,Fork right){
+		
+			if(left.forkUse==0 && right.forkUse==0){
+			try {
+				
+				
+				left.forkUse=1;
+				right.forkUse=1;
+				wait(50);
+				System.out.println("Philosopher "+Thread.currentThread().getName()+"is eating");
+				
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			}
+		
+	}
+	public synchronized void think(){
+		
+			
+			
+			
+			left.forkUse=0;
+			right.forkUse=0;
+			System.out.println("Philosopher"+Thread.currentThread().getName()+"is thinking");
+			notify();
+			
 		
 		
 	}
-
-
+	}
+class Fork{
+	int forkUse;
+	
+	Fork(){
+		forkUse=0;
+	}
+}
