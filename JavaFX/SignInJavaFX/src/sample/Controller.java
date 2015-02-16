ae9c595d99ad4b25ac2035f11ba309a810a2afb8
Royal Gulf <avdeshkumar@amiworks.com>
2015-02-16 16:46:43 +0530

    binded button,textfield,label

--- JavaFX/SignInJavaFX/src/sample/Controller.java
+++ JavaFX/SignInJavaFX/src/sample/Controller.java
@@ -1,21 +1,27 @@
 package sample;
+
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
+import javafx.fxml.Initializable;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-
-
-public class Controller {
+public class Controller implements Initializable {
+    @FXML public Button button;
     @FXML private TextField userName;
     @FXML private PasswordField passwordField;
+    @FXML private Label user;
 
     private WelcomeController welcomeController;
     private Scene scene;
@@ -39,11 +45,19 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }
+
     @FXML public void handleSubmitButtonAction(ActionEvent event)throws IOException{
         if(userName.getText().trim().length()>0){
             welcomeController =new WelcomeController();
             welcomeController.reDirectHome(stage,userName.getText().trim());
         }
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+         user.textProperty().bind(userName.textProperty());
+        button.disableProperty().bind(Bindings.or(userName.textProperty().isEqualTo(""),passwordField.textProperty().isEqualTo("")));
 
     }
 }
